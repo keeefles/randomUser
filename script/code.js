8ba1a7159c782521531a64bf6466ea3b753e1fc4
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-30 16:33:20 +0200

    what is even this

--- script/code.js
+++ script/code.js
@@ -1,40 +1,37 @@
-let friends = []
-function add(){
-    let name = document.querySelector('[data-entry]').value
-    friends.push(name)
+let order = []
+console.log();
 
-    localStorage.setItem('friendsNames',
-    JSON.stringify(friends))
-    displayFriends()
-}
-document.querySelector('[data-submit]').addEventListener('click', add)
-
-function displayFriends(){
-    let listWrapper = document.querySelector('[data-friends]');
-    let cFriends = JSON.parse(localStorage.getItem('friendNames'));
-    listWrapper.innerHTML = ""
-    if(cFriends) {
-        cFriends.forEach(people => {
-            listWrapper.innerHTML +=
-            `<li>${people}</li>`
-        });
-    }
-}
 let cardContainer = document.querySelector('[data-cards]');
-fetch('https://randomuser.me/api?results=20')
-.then(data=> data.json())
+fetch('https://randomuser.me/api?results=20').then(data=> data.json())
 .then(result=> {
-    let {results} = result
+    let {results} = result 
     results.forEach( people =>{
         console.log(people);
-        cardContainer.innerHTML +=
+        cardContainer.innerHTML += 
+
         `
         <div class="card" style="width: 18rem;">
             <img src="${people.picture.large}" class="card-img-top img-fluid" alt="${people.name.first}">
             <div class="card-body">
             <h5 class="card-title">${people.name.title}. ${people.name.first} ${people.name.last}</h5>
             <p class="card-text">Age: ${people.registered.age}</p>
-        </div>
+            </div>
         `
+
+order.push(people)
+
+        });
     });
+    document.querySelector('[data-submit2]').addEventListener('click', function ascend(){
+        document.querySelector('[data-submit1]').addEventListener('click', function searchName() {
+        
+            let personName = document.querySelector('[data-input]').value.toLowerCase();
+            let content = document.querySelector('[data-submit1]');
+            if (content.toLowerCase().includes(personName)) {
+                document.querySelector('[data-input]').innerHTML = personName + "found";
+            } else {
+                document.getElementById('result').innerHTML = "Name not found";
+            }
+
+})
 });
